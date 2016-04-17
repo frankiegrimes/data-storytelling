00f8b35673452580792dd33b7cfa834926c1e06d
frankiegrimes <frankjgrimes@gmail.com>
2016-04-17 22:26:35 +0100

    Added javascript sticky nav bar

---
+++ js/min/parallax-min.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(window).scroll(function(){$(window).scrollTop()>74&&($("#flex-nav-list").addClass("flex-nav-fixed"),$(".flex-content").addClass("flex-content-fixed")),$(window).scrollTop()<75&&($("#flex-nav-list").removeClass("flex-nav-fixed"),$(".flex-content").removeClass("flex-content-fixed"))})});
---
+++ js/parallax.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  
+  $(window).scroll(function () {
+      //if you hard code, then use console
+      //.log to determine when you want the 
+      //nav bar to stick.  
+    if ($(window).scrollTop() > 74) {
+      $('#flex-nav-list').addClass('flex-nav-fixed');
+      $('.flex-content').addClass('flex-content-fixed');
+    }
+    if ($(window).scrollTop() < 75) {
+      $('#flex-nav-list').removeClass('flex-nav-fixed');
+      $('.flex-content').removeClass('flex-content-fixed');
+    }
+  });
+});
