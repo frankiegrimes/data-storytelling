6733f59cfde1a6b97cb23dab108d6c65291781be
frankiegrimes <frankjgrimes@gmail.com>
2016-04-21 16:32:44 +0100

    (nearly) Finished responsively
    Updated CSS to fix responsivity

--- js/parallax.js
+++ js/parallax.js
@@ -1,18 +1,32 @@
 $(document).ready(function() {
 
-
+  $('.flex-footer').addClass('hidden');
   
   $(window).scroll(function () {
-      //if you hard code, then use console
-      //.log to determine when you want the 
-      //nav bar to stick.  
+      //stick nav-bar
     if ($(window).scrollTop() > 74) {
       $('#flex-nav-list').addClass('flex-nav-fixed');
       $('.flex-content').addClass('flex-content-fixed');
+      $('.flex-footer').removeClass('hidden');
+      
     }
     if ($(window).scrollTop() < 75) {
       $('#flex-nav-list').removeClass('flex-nav-fixed');
       $('.flex-content').removeClass('flex-content-fixed');
+      
+      $('.flex-footer').addClass('hidden');
     }
+
+    // remove for smaller devices
+    if (window.matchMedia('(max-width: 768px)').matches) {
+      $('#flex-nav-list').removeClass('flex-nav-fixed');
+      $('.flex-content').removeClass('flex-content-fixed');
+    }
+    if (window.matchMedia('(max-width: 992px)').matches) {
+      $('#flex-nav-list').removeClass('flex-nav-fixed');
+      $('.flex-content').removeClass('flex-content-fixed');
+    }
+    
+    
   });
 });
