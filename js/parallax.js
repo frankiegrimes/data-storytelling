1bef9d208cf61e37917fd360d25fc826a923d950
frankiegrimes <frankjgrimes@gmail.com>
2016-04-24 21:23:00 +0100

    Condensed into one page
    Added jquery active menu highlighting and smooth scrolling

--- js/parallax.js
+++ js/parallax.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function() {
 
   $('.flex-footer').addClass('hidden');
@@ -6,13 +8,13 @@ $(document).ready(function() {
   $(window).scroll(function () {
       //stick nav-bar
     if ($(window).scrollTop() > 95) {
-      $('#flex-nav-list').addClass('flex-nav-fixed');
+      $('.flex-nav-list').addClass('flex-nav-fixed');
       $('.flex-content').addClass('flex-content-fixed');
       $('.flex-footer').removeClass('hidden');
       
     }
     if ($(window).scrollTop() < 96) {
-      $('#flex-nav-list').removeClass('flex-nav-fixed');
+      $('.flex-nav-list').removeClass('flex-nav-fixed');
       $('.flex-content').removeClass('flex-content-fixed');
       
       $('.flex-footer').addClass('hidden');
@@ -20,17 +22,53 @@ $(document).ready(function() {
 
     // remove for smaller devices
     if (window.matchMedia('(max-width: 768px)').matches) {
-      $('#flex-nav-list').removeClass('flex-nav-fixed');
+      $('.flex-nav-list').removeClass('flex-nav-fixed');
       $('.flex-content').removeClass('flex-content-fixed');
     }
     if (window.matchMedia('(max-width: 992px)').matches) {
-      $('#flex-nav-list').removeClass('flex-nav-fixed');
+      $('.flex-nav-list').removeClass('flex-nav-fixed');
       $('.flex-content').removeClass('flex-content-fixed');
     }
     
     
   });
 
+$(document).on("scroll", onScroll);
+    
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+        
+        $('a').each(function () {
+            $(this).removeClass('active');
+        });
+        $(this).addClass('active');
+      
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+
 });
 
-
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('#flex-nav a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('#flex-nav a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
+}
